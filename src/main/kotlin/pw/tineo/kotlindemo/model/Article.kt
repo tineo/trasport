d59c17d53ea6cebd4526b6a0f0906e3113ef2a50
Sasuke Uchiha <cesar@tineo.mobi>
2018-12-02 17:54:23 -0500

    add seq articles

--- src/main/kotlin/pw/tineo/kotlindemo/model/Article.kt
+++ src/main/kotlin/pw/tineo/kotlindemo/model/Article.kt
@@ -2,13 +2,15 @@ package pw.tineo.kotlindemo.model
 
 import javax.persistence.Entity
 import javax.persistence.GeneratedValue
+import javax.persistence.SequenceGenerator
 import javax.persistence.GenerationType
 import javax.persistence.Id
 import javax.validation.constraints.NotBlank
 
 @Entity
 data class Article (
-        @Id @GeneratedValue(strategy = GenerationType.AUTO)
+        @Id @GeneratedValue(strategy = GenerationType.AUTO) @SequenceGenerator(name="seq_article_gen", sequenceName="seq_article")
+
         val id: Long = 0,
 
         @get: NotBlank
